8bb650421273cab67864707bc515df23db7d4309
Eric <eric092@163.com>
2021-01-23 19:00:32 +0800

    the ways of creating bean

---
+++ spring-parent/spring-annotation/src/main/java/com/eric/condition/WindowsCondition.java
@@ -0,0 +1,24 @@
+package com.eric.condition;
+
+import org.springframework.context.annotation.Condition;
+import org.springframework.context.annotation.ConditionContext;
+import org.springframework.core.env.Environment;
+import org.springframework.core.type.AnnotatedTypeMetadata;
+
+/**
+ * Description: spring-parent
+ *
+ * @author zhangxiusen
+ * @date 2021/1/23
+ */
+public class WindowsCondition implements Condition {
+    @Override
+    public boolean matches(ConditionContext conditionContext, AnnotatedTypeMetadata annotatedTypeMetadata) {
+        Environment environment = conditionContext.getEnvironment();
+        String osName = environment.getProperty("os.name");
+        if (osName.contains("Windows")){
+            return true;
+        }
+        return false;
+    }
+}
--- spring-parent/spring-annotation/src/test/java/com/eric/test/IOCTest.java
+++ spring-parent/spring-annotation/src/test/java/com/eric/test/IOCTest.java
@@ -1,6 +1,7 @@
 package com.eric.test;
 
 import com.eric.config.BeanConfig;
+import com.eric.config.BeanConfig2;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -20,5 +21,40 @@ public class IOCTest {
 		for (String name : beanNames){
 			System.out.println(name);
 		}
+	}
+
+	@Test
+	public void testLazy(){
+		ApplicationContext ac = new AnnotationConfigApplicationContext(BeanConfig2.class);
+		ac.getBean("eric");
+		ac.getBean("eric");
+		ac.getBean("bill");
+	}
+
+	/**
+	 * 给容器中注册组件：
+	 * 	1、包扫描+组件标注注解（@@ComponentScan + Controller/@Service/@Repository/@Component）
+	 * 	2、@Bean[导入的第三方包里面的组件]
+	 * 	3、@Import[快速给容器中导入一个组件]
+	 * 		1)、@Import(要导入容器中的组件)；容器中就会自动注册这个组件，id默认是全类名
+	 * 		2)、ImportSelector：返回需要导入的组件的全类名数组
+	 * 		3)、ImportSelectorDefinitionRegistrar：手动注册bean到容器中
+	 * 	4、调用FactoryBean创建Spring定义的bean
+	 * 		1)、默认获取到的是工厂bean调用getObject创建的对象
+	 * 		2)、要获取工厂bean本身，需要在id前面加一个&：&colorFactoryBean
+	 */
+	@Test
+	public void testCreateBean(){
+		ApplicationContext ac = new AnnotationConfigApplicationContext(BeanConfig2.class);
+		String[] beanNames = ac.getBeanDefinitionNames();
+		for (String name : beanNames){
+			System.out.println(name);
+		}
+
+		// 工厂bean获取的是调用betObject创建的对象
+		Object bean2 = ac.getBean("colorFactoryBean");
+		System.out.println("bean2的类型："+bean2.getClass());
+		Object bean3 = ac.getBean("&colorFactoryBean");
+		System.out.println("bean3的类型："+bean3.getClass());
 	}
 }
